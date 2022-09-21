7149cc7f176aca9841325b7fc9ebf9d1ee36cfae
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-21 15:26:09 +0200

    add interests

--- frontend/src/views/StartView.tsx
+++ frontend/src/views/StartView.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-type Props = {}
+type Props = {
+
+}
 
 const StartView = (props: Props) => {
+  const [newInterest, setNewInterest] = useState('');
+
+  async function addInterest() {
+    const user = localStorage.getItem('user');
+    const userInterestObject:object = {
+      user: user,
+      interest: newInterest
+    };
+    const response = await fetch('http://localhost:1234/addinterest', {
+      method: 'POST',
+      body: JSON.stringify(userInterestObject),
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+    console.log(data);
+    
+    
+  }
   return (
-    <div>StartView</div>
+    <div>
+      <h1>Välkommen</h1>
+      <section>
+        <label htmlFor="interest-input"></label>
+        <input onChange={(e) => setNewInterest(e.target.value)} type="text" id="interest-input" placeholder='Ange intresse'/>
+        <button onClick={addInterest}>Lägg till intresse</button>
+      </section>
+    </div>
   )
 }
 
