d662cd2d85629d2a576f75767cb9bdfb0ad941b9
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-09-21 14:29:24 +0200

    login done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,71 @@
 const express = require('express');
 const nedb = require('nedb-promise');
 const cors = require('cors');
+const app = express();
 
-// skapa konto
+// säg till servern att alla får utnjyttja vårt API
+app.use(cors({origin: '*'}));
+app.use(express.json());
+
+// en databas för konton
+const accountsDB = new nedb({filename: 'accounts.db', autoload: true});
+
+//en databas för intressen länkade till ett konto
+const interestsDB = new nedb({filename: 'interests.db', autoload: true});
+
+// skapa konto - endpoint
+app.post('/signup', async (request, response) => {
+    // vi kommer få in användarens anv.namn och lösen i en body
+    const credentials = request.body;
+    console.log(credentials)
+    const responseObject = {
+        success: true,
+        usernameExists: false
+    };
+    // kolla igenom databasen om användarnamnet som skickats in redan existerar
+    const usernameExists = await accountsDB.find({ username: credentials.usename });
+    // .find() returnerar en lista på alla träffar
+    if ( usernameExists.length > 0 ) {
+        responseObject.usernameExists = true;
+    }
+    if ( responseObject.usernameExists ) {
+        responseObject.success = false;
+    } else {
+        // om användarnamn är unikt tjoffa in i account.db
+        // hasha vårt lösenord vid bcrypt eller liknande
+        accountsDB.insert(credentials);
+    }
+    response.json(responseObject);
+});
 // logga in
+app.post('/login', async (request, response) => {
+    // vi kommer få in användarens anv.namn och lösen i en body
+    const credentials = request.body;
+
+    const responseObject = {
+        success: false,
+        user: '',
+        interests: []
+    }
+    console.log(credentials);
+    // kollar mot accounts.db om användare med namnet finns
+    const account = await accountsDB.find({$and: [{username: credentials.username}, {password: credentials.password}] }); 
+    if (account.length > 0) {
+
+            console.log('korrekt lösenord', account[0]);
+            responseObject.user = account[0].username;
+            responseObject.success = true;
+            // vi vill kolla om användaren har intressent
+            // om det finns; skicka med i responseObject
+    } else {
+        responseObject.success = false;
+    }
+    response.json(responseObject);
+});
 // lägga till intressen
 // hitta matchande intressen
+
+// starta servern
+app.listen(1234, () => {
+    console.log('Server is running on port 1234');
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
+
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  async function signup() {
+    const account:object = {
+      username: username,
+      password: password
+    };
+    const response = await fetch('http://localhost:1234/signup', {
+      method: 'POST',
+      body: JSON.stringify(account),
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+  async function login() {
+    const account:object = {
+      username: loginUsername,
+      password: loginPassword
+    };
+    const response = await fetch('http://localhost:1234/login', {
+      method: 'POST',
+      body: JSON.stringify(account),
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+    console.log(data);
+    navigate('/start');
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h2>Sign up</h2>
+        <label htmlFor="username">Username</label>
+        <input onChange={(e) => setUsername(e.target.value)} id='username' type="text" placeholder='Username' />
+        <label htmlFor="password">Password</label>
+        <input onChange={(e) => setPassword(e.target.value)} type="password" id="password" placeholder='Password' />
+        <button onClick={signup}>SIGN ME UP</button>
+      </section>
+      <section>
+        <h2>Log in</h2>
+        <label htmlFor="login-username">Username</label>
+        <input onChange={(e) => setLoginUsername(e.target.value)} id='login-sername' type="text" placeholder='Username' />
+        <label htmlFor="login-password">Password</label>
+        <input onChange={(e) => setLoginPassword(e.target.value)} type="password" id="login-password" placeholder='Password' />
+        <button onClick={login}>LOG IN</button>
+      </section>
+
     </div>
   );
 }
